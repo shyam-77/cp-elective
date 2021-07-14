ac88a8d4385a9e3ad226cc2846e4fab2f58a6abb
shyam-77 <72121828+shyam-77@users.noreply.github.com>
2021-07-14 16:27:15 +0530

    Add files via upload
    done2

---
+++ 06-eggcartons-Python/# Write the function eggCartons(eggs) th.py
@@ -0,0 +1,15 @@
+# Write the function eggCartons(eggs) that takes 
+# a non-negative integer number of eggs, and returns 
+# the smallest integer number of cartons required to hold 
+# that many eggs, where a carton may hold up to 12 eggs
+
+
+def fun_eggcartons(eggs):
+	 if(eggs<=0):
+          return 0
+	 elif(eggs>0 and eggs<=12):
+		 return 1
+	 elif(eggs>12 and eggs<=24):
+	  	 return 2 
+	 elif(eggs>24 and eggs<=30):
+		 return 3
