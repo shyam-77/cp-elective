01064f1f871f4e220721ec4ca6820a5b2f7e320f
shyamsunder <shyamsunderpraja7777@msitprogram.net>
2021-07-14 17:19:10 +0530

    done2

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -5,5 +5,21 @@
 
 
 
+from attr import resolve_types
+
+
 def fun_isfactor(f, n):
-	return False # replace with your solution
+    if n==0 and f==0:
+        return True
+    elif f==0:
+        return False
+    elif n==0:
+        return True
+    
+    elif  n%f==0:
+        return True
+    else  :
+        return False
+    
+   
+    
