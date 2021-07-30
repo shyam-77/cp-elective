d4dfc5125537849e0dc5f61990f894bd38ab5f67
shyamsunder <shyamsunderpraja7777@msitprogram.net>
2021-07-30 09:41:59 +0530

    done

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,42 @@
 
 
 
+def isprime(x):
+    count=0
+    if x>1:
+        for i in range(1,x+1):
+            if(x%i)==0:
+                count = count+1
+        if count == 2:
+            return True
+        else:
+            return False
+  
+def numSquareSum(n):
+    squareSum = 0;
+    while(n>0):
+        squareSum += (n % 10) * (n % 10);
+        n = int(n / 10);
+    return squareSum
+def ishappynumber(n):
+    slow = n;
+    fast = n;
+    while(True):
+        slow = numSquareSum(slow);
+        fast = numSquareSum(numSquareSum(fast));
+        if(slow != fast):
+            continue;
+        else:
+            break;
+    if (slow == 1):
+        return True
+    else:
+        return False
 def fun_nth_happy_prime(n):
-	return 0
+    found = 0
+    guess = 0
+    while (found <= n):
+        guess += 1
+        if ((ishappynumber(guess)) and (isprime(guess))):
+            found += 1
+    return guess
