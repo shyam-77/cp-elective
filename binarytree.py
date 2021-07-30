e3171dd201438ce5f7c636378767cd2adcb12b1b
shyam-77 <72121828+shyam-77@users.noreply.github.com>
2021-07-30 15:01:35 +0530

    Add files via upload
    done

---
+++ binarytree.py
@@ -0,0 +1,40 @@
+class Node(object):
+    def _init_(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree(object):
+    def _init_(self, root):
+        self.root = Node(root)
+
+    def search(self, find_val):
+        """
+        Return True if the find_val is in the tree and False otherwise.
+        """
+        # Your code goes here
+        return self.preorder_search(self.root,find_val)
+
+    def print_tree(self):
+        """
+        Print out all tree nodes as they are visited in a pre-order traversal."""
+        # Your code goes here
+        return self.preorder_print(self.root,"")[:-1]
+
+    def preorder_search(self, start, find_val):
+        """
+        Helper method - use this to create a recursive search solution.
+        """
+        # Your code goes here
+        return start.value == find_val or self.preorder_search(start.right,find_val) if start else False
+
+    def preorder_print(self, start, traversal):
+        """
+        Helper method - use this to create a recursive print solution.
+        """
+        # Your code goes here
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left,traversal)
+            traversal = self.preorder_print(start.right,traversal)
+            return traversal
